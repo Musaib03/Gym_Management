6ec61bd705c5b15e9db2fe4c28b78c0a955e1dc1
Musaib03 <wanimusaib520@gmail.com>
2025-02-24 18:02:52 +0530

    modified

--- gymapp/urls.py
+++ gymapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about', views.about_view, name='about'),
+    path('membership-form/', views.membership_form, name='membership_form'),
+    path('membership-success/', views.membership_success, name='membership_success'),
 ]
